b840520baea99896de2dc439c3181e87a2e44f14
Jan Fidor <jan.p.fidor@gmail.com>
2024-08-25 12:49:16 +0200

    add random augments

--- train_covid.py
+++ train_covid.py
@@ -146,6 +146,21 @@ def get_train_augmentations(name):
             v2.Resize(224),
             v2.RandomRotation(10),
             v2.RandomHorizontalFlip(0.5)
+        ]),
+        "weak-random": v2.Compose([
+            v2.RandomResizedCrop(224, 0.95),
+            v2.RandomRotation(10),
+            v2.RandomHorizontalFlip(0.5)
+        ]),
+        "medium-random": v2.Compose([
+            v2.RandomResizedCrop(224, 0.85),
+            v2.RandomRotation(10),
+            v2.RandomHorizontalFlip(0.5)
+        ]),
+        "strong-random": v2.Compose([
+            v2.RandomResizedCrop(224, 0.75),
+            v2.RandomRotation(10),
+            v2.RandomHorizontalFlip(0.5)
         ])
     }.get(name, v2.Identity())
 
