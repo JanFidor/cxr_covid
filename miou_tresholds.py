dcfafbed2cb41c66f4bab8ce0120f3c1d81deb31
Jan Fidor <jan.p.fidor@gmail.com>
2024-08-25 14:47:27 +0200

    use batching with miou thresholds

--- miou_tresholds.py
+++ miou_tresholds.py
@@ -18,6 +18,18 @@ from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
 from torchmetrics import JaccardIndex
 from torchmetrics import MeanMetric
+from torchvision.transforms import v2
+from torch.utils.data import DataLoader
+from datasets import (
+    GitHubCOVIDDataset,
+    BIMCVCOVIDDataset,
+    ChestXray14Dataset,
+    PadChestDataset,
+    BIMCVNegativeDataset, 
+    DomainConfoundedDataset
+)
+
+BATCH_SIZE = 1
 
 def load_model(model_path):
     cpt = torch.load(model_path, weights_only=False)
@@ -29,7 +41,23 @@ def get_gradcam_layers(model):
     elif isinstance(model, DenseNet):
         return [model.features[-2].denselayer16.conv2]
     raise KeyError
-        
+
+def get_preprocessing(name):
+    return {
+        "weak": v2.Compose([
+            v2.CenterCrop(int(224 * 0.95)),
+            v2.Resize(224),
+        ]),
+        "medium": v2.Compose([
+            v2.CenterCrop(int(224 * 0.85)),
+            v2.Resize(224),
+        ]),
+        "strong": v2.Compose([
+            v2.CenterCrop(int(224 * 0.75)),
+            v2.Resize(224),
+        ])
+    }.get(name, v2.Identity())
+
 def get_gradcams(gradcam_names, model_path):
     model = load_model(model_path)
     gradcam_layers = get_gradcam_layers(model)
@@ -46,29 +74,33 @@ def get_gradcams(gradcam_names, model_path):
 def model_name_from_path(model_path):
     return model_path
 
-def generate_localisation(threshold, gradcam, path):
-    tensor = torch.load(path, weights_only=True)
-    
+def generate_localisation(threshold, gradcam, tensors):
     saliency_map = gradcam(
-        input_tensor=tensor.unsqueeze(0),
+        input_tensor=tensors,
         targets=[ClassifierOutputTarget(1)]
     )
 
     return torch.tensor(saliency_map > threshold)
 
-def calculate_saliency_jaccard(threshold, gradcams1, gradcams2, image_paths):
+def calculate_saliency_jaccard(threshold, gradcams1, gradcams2, dataloader):
     jaccard = JaccardIndex(task='multiclass', num_classes=2)
     mean_jaccard = MeanMetric()
-    for path in image_paths:
+    for tensors, _, _, _ in dataloader:
         for cam1, cam2 in zip(gradcams1, gradcams2):
-            att1 = generate_localisation(threshold, cam1, path)
-            att2 = generate_localisation(threshold, cam2, path)
+            att1 = generate_localisation(threshold, cam1, tensors)
+            att2 = generate_localisation(threshold, cam2, tensors)
 
             iou = jaccard(att1, att2)
             mean_jaccard.update(iou)
     return mean_jaccard.compute().item()
 
-def create_miou_matrix(threshold, gradcam_names, model_paths, image_paths):
+def create_miou_matrix(threshold, gradcam_names, model_paths, dataset):
+    dataloader = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=0,
+    )
     all_gradcams = [
         get_gradcams(gradcam_names, path) for path in model_paths
     ]
@@ -80,18 +112,18 @@ def create_miou_matrix(threshold, gradcam_names, model_paths, image_paths):
         for ii in range(d):
             cam1 = all_gradcams[i]
             cam2 = all_gradcams[ii]
-            matrix[i][ii] = calculate_saliency_jaccard(threshold, cam1, cam2, image_paths)
+            matrix[i][ii] = calculate_saliency_jaccard(threshold, cam1, cam2, dataloader)
     return matrix
 
-def creat_heatmap(save_path, threshold, gradcam_names, model_paths, image_paths, legend=False):
-    matrix = create_miou_matrix(threshold, gradcam_names, model_paths, image_paths)
+def create_heatmap(save_path, threshold, gradcam_names, model_paths, dataloader, legend=False):
+    matrix = create_miou_matrix(threshold, gradcam_names, model_paths, dataloader)
     
     plot = sn.heatmap(matrix, annot=True, cbar=legend)
     fig = plot.get_figure()
     fig.savefig(save_path) 
     sn.reset_defaults()
 
-def miou_tresholds(split_path, heatmap_name, threshold, gradcam_names, model_paths):
+def miou_tresholds(preprocess, split_path, heatmap_name, threshold, gradcam_names, model_paths):
     # threshold = 0.7
     # gradcam_names = ["grad++_cam", "eigen_cam"]
     # heatmap_name = ""
@@ -100,42 +132,32 @@ def miou_tresholds(split_path, heatmap_name, threshold, gradcam_names, model_pat
     # ]
 
     # split_path = "42/dataset3"
-    rootdir = Path("splits", split_path)
 
-    traindfs = {}
-    valdfs = {}
-    for path in rootdir.rglob("*"):
-        image_names = pd.read_csv(path).path
-        dataset_dir = "bimcv+" if path.stem.split('-')[0] == 'positive' else 'bimcv-'
-        image_paths = [
-            f"data/tensors/{dataset_dir}/{Path(name).stem}.pt" for name in image_names
-        ]
-        if path.stem.endswith("val"):
-            valdfs[path.stem] = image_paths[:2]
-        else:
-            traindfs[path.stem] = image_paths[:2]
+    augments = get_preprocessing(preprocess)
+
+    trainds = DomainConfoundedDataset(
+            BIMCVNegativeDataset(fold='all', augments=augments, labels='chestx-ray14'),
+            BIMCVCOVIDDataset(fold='all', augments=augments, labels='chestx-ray14')
+            )
+    valds = DomainConfoundedDataset(
+            BIMCVNegativeDataset(fold='all', labels='chestx-ray14', augments=augments),
+            BIMCVCOVIDDataset(fold='all', labels='chestx-ray14', augments=augments)
+            )
+    
+    split_dir = f"splits/{split_path}"
+    trainds.ds1.df = pd.read_csv(f"{split_dir}/negative-train.csv")
+    valds.ds1.df = pd.read_csv(f"{split_dir}/negative-val.csv")
+
+    trainds.ds2.df = pd.read_csv(f"{split_dir}/positive-train.csv")
+    valds.ds2.df = pd.read_csv(f"{split_dir}/positive-val.csv")
 
     save_dir = f"examples/heatmaps/{split_path}/{heatmap_name}"
     Path(save_dir).mkdir(parents=True, exist_ok=True)
 
-    paths = []
-    for curr in traindfs.values():
-        paths += curr
-    save_path = f"{save_dir}/combined-train.png"
-    creat_heatmap(save_path, threshold, gradcam_names, model_paths, paths, True)
-
-    paths = []
-    for curr in valdfs.values():
-        paths += curr
-    save_path = f"{save_dir}/combined-val.png"
-    creat_heatmap(save_path, threshold, gradcam_names, model_paths, paths)
-
-    for name, paths in traindfs.items():
-        save_path = f"{save_dir}/{name}.png"
-        creat_heatmap(save_path, threshold, gradcam_names, model_paths, paths)
-    for name, paths in valdfs.items():
-        save_path = f"{save_dir}/{name}.png"
-        creat_heatmap(save_path, threshold, gradcam_names, model_paths, paths)
+    # create_heatmap(f"{save_dir}/negative-train", threshold, gradcam_names, model_paths, trainds.ds1, True)
+    # create_heatmap(f"{save_dir}/positive-train", threshold, gradcam_names, model_paths, trainds.ds2, True)
+    create_heatmap(f"{save_dir}/negative-val", threshold, gradcam_names, model_paths, valds.ds1, True)
+    create_heatmap(f"{save_dir}/positive-val", threshold, gradcam_names, model_paths, valds.ds2, True)
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser(
@@ -155,4 +177,4 @@ if __name__ == "__main__":
     ]
 
     split_path = "42/dataset3"
-    miou_tresholds(split_path, heatmap_name, threshold, gradcam_names, model_paths)
+    miou_tresholds("strong", split_path, heatmap_name, threshold, gradcam_names, model_paths)
